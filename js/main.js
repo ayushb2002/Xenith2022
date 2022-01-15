81e325e513641fefe04e6605db016001961fb5e0
Avni17 <avni.arora2002@gmail.com>
2022-01-15 22:28:06 +0530

    avni

--- js/main.js
+++ js/main.js
@@ -134,28 +134,28 @@ fetch("./sponsorData.json")
 
 var header = document.getElementById('pos-rel-1');
 
-function fadeOutOnScroll(element) {
-  if (!element) {
-    return;
-  }
+// function fadeOutOnScroll(element) {
+//   if (!element) {
+//     return;
+//   }
 
-  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-  var elementHeight = element.offsetHeight;
-  var scrollTop = document.documentElement.scrollTop;
+//   var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+//   var elementHeight = element.offsetHeight;
+//   var scrollTop = document.documentElement.scrollTop;
 
-  var opacity = 1;
+//   var opacity = 1;
 
-  if (scrollTop > distanceToTop) {
-    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-  }
+//   if (scrollTop > distanceToTop) {
+//     opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+//   }
 
-  if (opacity >= 0) {
-    element.style.opacity = opacity;
-  }
-}
+//   if (opacity >= 0) {
+//     element.style.opacity = opacity;
+//   }
+// }
 
-function scrollHandler() {
-  fadeOutOnScroll(header);
-}
+// function scrollHandler() {
+//   fadeOutOnScroll(header);
+// }
 
-window.addEventListener('scroll', scrollHandler);
+// window.addEventListener('scroll', scrollHandler);
